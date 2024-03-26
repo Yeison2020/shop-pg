6b61e813268b8ca45fc306b4c4d16cd8b360ec87
Yeison Casado <72528124+Yeison2020@users.noreply.github.com>
2024-03-26 13:56:48 -0400

    shop-pg

--- tracer/tracer.ts
+++ tracer/tracer.ts
@@ -3,12 +3,7 @@ import tracer from 'dd-trace';
 tracer.init({
   env: 'dev',
   service: 'shop-app',
-  serviceMapping: {
-    "postgres:'shop-app-postgres": 'shop-app',
-  },
-  ingestion: {
-    sampleRate: 1,
-  },
+  port: '8136',
 });
 
 export default tracer;
